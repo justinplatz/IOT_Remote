df241e603d2759b877433f6250e566d9fd4618e2
Justin Platz <jplatz@umich.edu>
2016-02-21 20:35:34 -0500

    Add Color File
    This file will be used for all colors that we use throughout the app. We can more easily access and call these colors by initializing them within this file

---
+++ IOTRemote/UIColor+IOTRemote_Colors.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+IOTRemote_Colors.h
+//  IOTRemote
+//
+//  Created by Justin Platz on 2/21/16.
+//  Copyright © 2016 ioJP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (IOTRemote_Colors)
+
++ (UIColor *)lightYellowColor;
++ (UIColor *)offGrayColor;
+
+@end
---
+++ IOTRemote/UIColor+IOTRemote_Colors.m
@@ -0,0 +1,24 @@
+//
+//  UIColor+IOTRemote_Colors.m
+//  IOTRemote
+//
+//  Created by Justin Platz on 2/21/16.
+//  Copyright © 2016 ioJP. All rights reserved.
+//
+
+#import "UIColor+IOTRemote_Colors.h"
+
+@implementation UIColor (IOTRemote_Colors)
+
++ (UIColor *)lightYellowColor
+{
+    return [UIColor colorWithRed:0.922 green:0.918 blue:0.612 alpha:1]; /*#ebea9c*/
+}
+
++ (UIColor *)offGrayColor
+{
+    return [UIColor colorWithRed:0.741 green:0.765 blue:0.78 alpha:1]; /*#bdc3c7*/
+}
+
+
+@end
