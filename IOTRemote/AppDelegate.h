c33cb531deb3019ea102e3e3db3cc448ee355b43
Justin Platz <jplatz@umich.edu>
2016-02-21 20:16:50 -0500

    First commit, create project

---
+++ IOTRemote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOTRemote
+//
+//  Created by Justin Platz on 2/21/16.
+//  Copyright © 2016 ioJP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOTRemote/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOTRemote
+//
+//  Created by Justin Platz on 2/21/16.
+//  Copyright © 2016 ioJP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
