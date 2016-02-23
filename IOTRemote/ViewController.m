75cbd0b9beabc873ad4ae7716d97735db3f79bdb
Justin Platz <jplatz@umich.edu>
2016-02-23 12:51:56 -0500

    Delete unnecessary comments from code

--- IOTRemote/ViewController.m
+++ IOTRemote/ViewController.m
@@ -34,7 +34,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     self.lightIsOn = NO;
     self.fanButton = NO;
     self.discoIsOn = NO;
