3d424411a07148269c262d408720dfb0e1733aba
Justin Platz <jplatz@umich.edu>
2016-03-07 20:54:42 -0500

    Update Images in app

---
+++ MyViewControllerDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyViewControllerDelegate.h
+//  IOTRemote
+//
+//  Created by Justin Platz on 3/7/16.
+//  Copyright © 2016 ioJP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@protocol MyViewControllerDelegate
+
+@property (nonatomic) ViewController *viewController;
+
+@end
