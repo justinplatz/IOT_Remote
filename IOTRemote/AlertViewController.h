eeed234663a17c40ef8edec80ca964814179a5ed
Justin Platz <jplatz@umich.edu>
2016-04-10 17:38:12 -0400

    Add / Remove images, create AlertViewController

---
+++ IOTRemote/AlertViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+AlertViewController.h
+//  IOTRemote
+//
+//  Created by Justin Platz on 4/9/16.
+//  Copyright © 2016 ioJP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (AlertViewController)
+
+@end
---
+++ IOTRemote/AlertViewController.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+AlertViewController.m
+//  IOTRemote
+//
+//  Created by Justin Platz on 4/9/16.
+//  Copyright © 2016 ioJP. All rights reserved.
+//
+
+#import "UIViewController+AlertViewController.h"
+
+@implementation UIViewController (AlertViewController)
+
+@end
