7e27c431ba45ba628fbd65a2d9af6529c03e94c9
Justin Platz <jplatz@umich.edu>
2016-04-13 18:50:13 -0400

    Connect all WeMos and implement success/fail funcs

--- IOTRemote/ViewController.m
+++ IOTRemote/ViewController.m
@@ -40,26 +40,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.lightIsOn = NO;
-    self.fanButton = NO;
-    self.discoIsOn = NO;
-    
-    
     [self addBordersToViews];
     [self setAllViewsToGrayBackground];
     
+    NSString *Switch1 = [[NSUserDefaults standardUserDefaults]
+                            stringForKey:@"Switch1"];
+    if ([Switch1 containsString:@"On"]) {
+        [self turnLightButtonOn];
+    }
+    else{
+        [self turnLightButtonOff];
+    }
     
-    // Override point for customization after application launch.
-    PNConfiguration *configuration = [
-                                      PNConfiguration configurationWithPublishKey:@"pub-c-f83b8b34-5dbc-4502-ac34-5073f2382d96"
-                                      subscribeKey:@"sub-c-34be47b2-f776-11e4-b559-0619f8945a4f"];
-    
-    self.client = [PubNub clientWithConfiguration:configuration];
-    [self.client addListener:self];
-    
-    // Put your APIKey
-    NSString *APIKey = @"7e687c42-00a4-488b-bec0-614eca45bf75";
-    [YO startWithAPIKey:APIKey];
+    NSString *Switch2 = [[NSUserDefaults standardUserDefaults]
+                         stringForKey:@"Switch2"];
+    if ([Switch2 containsString:@"On"]) {
+        [self turnFanButtonOn];
+    }
+    else{
+        [self turnFanButtonOff];
+    }
+
+    NSString *Switch3 = [[NSUserDefaults standardUserDefaults]
+                         stringForKey:@"Switch3"];
+    if ([Switch3 containsString:@"On"]) {
+        [self turnDiscoButtonOn];
+    }
+    else{
+        [self turnDiscoButtonOff];
+    }
     
 }
 
@@ -109,24 +118,26 @@
     self.discoView.backgroundColor = [UIColor offGrayColor];
 }
 
+-(void) turnLightButtonOff{
+    [self.lightButton setImage:[UIImage imageNamed:@"light_off.png"] forState:UIControlStateNormal];
+    self.lightIsOn = NO;
+    self.lightView.backgroundColor = [UIColor offGrayColor];
+}
+
+-(void) turnLightButtonOn{
+    [self.lightButton setImage:[UIImage imageNamed:@"light_on.png"] forState:UIControlStateNormal];
+    self.lightIsOn = YES;
+    self.lightView.backgroundColor = [UIColor saffronYellow];
+}
+
 - (IBAction)lightButtonPressed:(id)sender {
     [self playClickSound];
-    [self.client publish:@"Test" toChannel: @"light_channel" storeInHistory:YES
-          withCompletion:nil];
-    
-    // Yo Individual Usernames
-    [YO sendYOToIndividualUser:@"IFTTT"];
-    
     
     if (self.lightIsOn) {
-        [self.lightButton setImage:[UIImage imageNamed:@"light_off.png"] forState:UIControlStateNormal];
-        self.lightIsOn = NO;
-        self.lightView.backgroundColor = [UIColor offGrayColor];
+        [self sendMessageToTwilio:@"#Switch1Off"];
     }
     else{
-        [self.lightButton setImage:[UIImage imageNamed:@"light_on.png"] forState:UIControlStateNormal];
-        self.lightIsOn = YES;
-        self.lightView.backgroundColor = [UIColor saffronYellow];
+        [self sendMessageToTwilio:@"#Switch1On"];
     }
 }
 
@@ -134,45 +145,13 @@
     [self playClickSound];
 }
 
-- (void)sendMessage{
-    NSString *kTwilioSID = @"ACf47ab7021aa158498f4fdb0bfc7685cb";
-    NSString *kTwilioSecret = @"643e1be96d15f39bcc7f55cd92ef35c8";
-    NSString *kFromNumber = @"+17084773770";
-    NSString *kToNumber = @"+15164763716";//replace you number here
-    NSString *kMessage = @"Tommy needs help!";
-    
-    NSString *urlString = [NSString
-                           stringWithFormat:@"https://%@:%@@api.twilio.com/2010-04-01/Accounts/%@/SMS/Messages/",
-                           kTwilioSID, kTwilioSecret,kTwilioSID];
-    
-    NSDictionary* dic=@{@"From":kFromNumber,@"To":kToNumber,@"Body":kMessage};
-    
-    __block NSArray* jsonArray;
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    manager.responseSerializer=[AFHTTPResponseSerializer serializer];
-    manager.responseSerializer.acceptableContentTypes=[NSSet setWithObject:@"application/xml"];
-    [manager POST:urlString parameters:dic success:^(AFHTTPRequestOperation *operation, id responseObject)
-     {
-         NSError* err;
-         NSLog(@"success %@",[[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]);
-         jsonArray=[NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingAllowFragments
-                                                     error:&err];
-         NSLog(@"JSON: %@", jsonArray);
-         
-         
-     } failure:^(AFHTTPRequestOperation *operation, NSError *error)
-     {
-         NSLog(@"Error: %@, %@", error, operation.responseObject);
-     }];
-    
-}
-
-- (void)turnLight{
-    NSString *kTwilioSID = @"ACf47ab7021aa158498f4fdb0bfc7685cb";
-    NSString *kTwilioSecret = @"643e1be96d15f39bcc7f55cd92ef35c8";
-    NSString *kFromNumber = @"+17084773770";
+//7jcbovka54f6o
+- (void)sendMessageToTwilio: (NSString*) message{
+    NSString *kTwilioSID = @"AC8c0a5caf4665a25c6d0fd0a63a7ea505";
+    NSString *kTwilioSecret = @"3eafdb27571da26adcac96e2a35ec961";
+    NSString *kFromNumber = @"+15165003260";
     NSString *kToNumber = @"+14156826723";//replace you number here
-    NSString *kMessage = @"turn light";
+    NSString *kMessage = message;
     
     NSString *urlString = [NSString
                            stringWithFormat:@"https://%@:%@@api.twilio.com/2010-04-01/Accounts/%@/SMS/Messages/",
@@ -191,57 +170,67 @@
          jsonArray=[NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingAllowFragments
                                                      error:&err];
          NSLog(@"JSON: %@", jsonArray);
+         [self showSuccessImage];
+         if ([message containsString:@"1"]) {
+             [[NSUserDefaults standardUserDefaults] setObject:message forKey:@"Switch1"];
+             if ([message containsString:@"On"]) {
+                 [self turnLightButtonOn];
+             }
+             else{
+                 [self turnLightButtonOff];
+             }
+         }
+         else if ([message containsString:@"2"]) {
+             [[NSUserDefaults standardUserDefaults] setObject:message forKey:@"Switch2"];
+             if ([message containsString:@"On"]) {
+                 [self turnFanButtonOn];
+             }
+             else{
+                 [self turnFanButtonOff];
+             }
+         }
+         else if ([message containsString:@"3"]) {
+             [[NSUserDefaults standardUserDefaults] setObject:message forKey:@"Switch3"];
+             if ([message containsString:@"On"]) {
+                 [self turnDiscoButtonOn];
+             }
+             else{
+                 [self turnDiscoButtonOff];
+             }
+         }
+         else{
+             //ERROR I DONT RECOGNIZE THIS SWITCH!!!!
+         }
+         [[NSUserDefaults standardUserDefaults] synchronize];
          
          
      } failure:^(AFHTTPRequestOperation *operation, NSError *error)
      {
          NSLog(@"Error: %@, %@", error, jsonArray);
+         [self showErrorImage];
      }];
     
 }
 
-
-
-- (void)buttonDidLongPress:(UILongPressGestureRecognizer*)gesture
-{
-    switch (gesture.state) {
-        case UIGestureRecognizerStateBegan:
-        {
-            
-            self.timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(alertButtonPressed:) userInfo:nil repeats:YES];
-            
-            NSRunLoop * theRunLoop = [NSRunLoop currentRunLoop];
-            [theRunLoop addTimer:self.timer forMode:NSDefaultRunLoopMode];
-            [self playClickSound];
-            [self disableButtonTemporarily];
-            [self sendMessage];
-
-        }
-            break;
-        case UIGestureRecognizerStateEnded:
-        {
-            [self.timer invalidate];
-            self.timer = nil;
-            
-        }
-            break;
-        default:
-            break;
-    }
+-(void) turnFanButtonOn{
+    [self.fanButton setImage:[UIImage imageNamed:@"fan_on.png"] forState:UIControlStateNormal];
+    self.fanIsOn = YES;
+    self.fanView.backgroundColor = [UIColor shamrockGreen];
 }
 
+-(void) turnFanButtonOff{
+    [self.fanButton setImage:[UIImage imageNamed:@"fan_off.png"] forState:UIControlStateNormal];
+    self.fanIsOn = NO;
+    self.fanView.backgroundColor = [UIColor offGrayColor];
+}
 
 - (IBAction)fanButtonPressed:(id)sender {
     [self playClickSound];
     if (self.fanIsOn) {
-        [self.fanButton setImage:[UIImage imageNamed:@"fan_off.png"] forState:UIControlStateNormal];
-        self.fanIsOn = NO;
-        self.fanView.backgroundColor = [UIColor offGrayColor];
+        [self sendMessageToTwilio:@"#Switch2Off"];
     }
     else{
-        [self.fanButton setImage:[UIImage imageNamed:@"fan_on.png"] forState:UIControlStateNormal];
-        self.fanIsOn = YES;
-        self.fanView.backgroundColor = [UIColor shamrockGreen];
+        [self sendMessageToTwilio:@"#Switch2On"];
     }
 }
 
@@ -253,19 +242,64 @@
     
 }
 
+-(void) turnDiscoButtonOn{
+    [self.discoButton setImage:[UIImage imageNamed:@"disco_on.png"] forState:UIControlStateNormal];
+    self.discoIsOn = YES;
+    self.discoView.backgroundColor = [UIColor californiaYellow];
+}
+
+-(void) turnDiscoButtonOff{
+    [self.discoButton setImage:[UIImage imageNamed:@"disco_off.png"] forState:UIControlStateNormal];
+    self.discoIsOn = NO;
+    self.discoView.backgroundColor = [UIColor offGrayColor];
+}
+
 - (IBAction)discoButtonPressed:(id)sender {
     [self playClickSound];
     if (self.discoIsOn) {
-        [self.discoButton setImage:[UIImage imageNamed:@"disco_off.png"] forState:UIControlStateNormal];
-        self.discoIsOn = NO;
-        self.discoView.backgroundColor = [UIColor offGrayColor];
+        [self sendMessageToTwilio:@"#Switch3Off"];
     }
     else{
-        [self.discoButton setImage:[UIImage imageNamed:@"disco_on.png"] forState:UIControlStateNormal];
-        self.discoIsOn = YES;
-        self.discoView.backgroundColor = [UIColor californiaYellow];
+        [self sendMessageToTwilio:@"#Switch3On"];
     }
 }
 
+-(void)showErrorImage{
+    UIImageView *overlayView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,self.view.frame.size.width,self.view.frame.size.height)];
+    overlayView.image = [UIImage imageNamed:@"messageFail.png"];
+    overlayView.backgroundColor = [UIColor whiteSmoke];
+    
+    CATransition *transition = [CATransition animation];
+    transition.duration = 1.5;
+    transition.type = kCATransitionPush; //choose your animation
+    [overlayView.layer addAnimation:transition forKey:nil];
+    
+    [self.view.window addSubview:overlayView];
+    
+    double delayInSeconds = 5.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds *   NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [overlayView removeFromSuperview];
+    });
+}
+
+-(void)showSuccessImage{
+    UIImageView *overlayView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,self.view.frame.size.width,self.view.frame.size.height)];
+    overlayView.image = [UIImage imageNamed:@"messageSent.png"];
+    overlayView.backgroundColor = [UIColor whiteSmoke];
+    
+    CATransition *transition = [CATransition animation];
+    transition.duration = 1.5;
+    transition.type = kCATransitionPush; //choose your animation
+    [overlayView.layer addAnimation:transition forKey:nil];
+    
+    [self.view.window addSubview:overlayView];
+    
+    double delayInSeconds = 5.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds *   NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [overlayView removeFromSuperview];
+    });
+}
 
 @end
